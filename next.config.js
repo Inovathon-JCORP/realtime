70e4cd9192bfc095c08cd7d034c70b98b1741634
Ramon <ramon.moreira_storm@prestador.globo>
2022-09-28 15:17:33 -0300

    chore: initial setup prisma

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
-// eslint-disable
 /** @type {import('next').NextConfig} */
-const withPwa = require('next-pwa');
 
-const nextConfig = withPwa({
+const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    dest: 'public',
-  },
-});
+};
 
 module.exports = nextConfig;
